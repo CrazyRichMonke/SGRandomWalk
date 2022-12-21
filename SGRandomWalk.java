6c16b7d2a22679af817def12086f3af070d7cbc6
CrazyRichMonke <99268999+CrazyRichMonke@users.noreply.github.com>
2022-12-21 08:56:55 -0500

    Update SGRandomWalk.java

--- SGRandomWalk.java
+++ SGRandomWalk.java
@@ -1,16 +1,33 @@
-public class TestClass {
+public class SGRandomWalk {
+    private static int steps;
     public static void main (String[]args){
-        Rectangle box = new Rectangle(100, 100, 300, 300);
+        int deltaX = (int)((Math.random()*21)-10);
+        int deltaY = (int)((Math.random()*11)-10);
+        boolean insideCircle = true;
+        Rectangle box = new Rectangle(0, 0, 300, 300);
         box.setColor(Color.BLUE);
         box.fill();  
-        box.draw();
-        Ellipse RandomWalk = new Ellipse(200, 200, 10, 10);
+        Ellipse RandomWalk = new Ellipse(150, 150, 10, 10);
         RandomWalk.setColor(Color.YELLOW);
         RandomWalk.fill();
-        Ellipse egg = new Ellipse(150, 150, 100, 100);
+        Ellipse egg = new Ellipse(50, 50, 200, 200);
         egg.setColor(Color.RED);
         egg.fill();  
-        egg.draw();   
+        egg.draw();
+
+        while(insideCircle){
+            RandomWalk.translate(deltaX, deltaY);
+            steps++;
+            try { Thread.sleep(100);}
+            catch(Exception ex) {};
+            deltaX = (int)((Math.random()*21)-10);
+            deltaY = (int)((Math.random()*11)-10);
+
+            
+        }
+
+        
+        
     }
     
 }
